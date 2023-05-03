6961f5396ab5dee542d1576c77a217fc1c4bbb9f
Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>
2023-05-03 14:30:12 +0200

    Implement fake repositories and models
    Signed-off-by: Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>

---
+++ app/src/main/java/com/tugraz/studybuddy/data/repository/CardRepository.java
@@ -0,0 +1,47 @@
+package com.tugraz.studybuddy.data.repository;
+
+import com.tugraz.studybuddy.data.model.CardModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardRepository implements ICardRepository<CardModel> {
+    private static final String CARD_COLLECTION = "cards";
+    private static final String TAG = "CardRepository";
+
+    private final List<CardModel> cards;
+
+    public CardRepository() {
+        cards = new ArrayList<>(List.of(
+                new CardModel("What is 1+1?", "2"),
+                new CardModel("What is 1+2?", "3"),
+                new CardModel("What is 1+3?", "4"),
+                new CardModel("What is 1+4?", "5")
+        ));
+    }
+
+    @Override
+    public List<CardModel> getAll() {
+        return cards;
+    }
+
+    @Override
+    public CardModel getById(String id) {
+        return cards.stream()
+                .filter(x -> x.getId().equals(id))
+                .findFirst().orElseGet(null);
+    }
+
+    @Override
+    public void add(CardModel entity) {
+        cards.add(entity);
+    }
+
+    @Override
+    public void update(CardModel entity) {
+    }
+
+    @Override
+    public void delete(CardModel entity) {
+    }
+}
