9d5a7c76f29fdd7f787238d1d863a259847cf921
maierbrf <fabio.maierbrugger@student.tugraz.at>
2023-05-03 16:53:50 +0200

    first steps for add with Viewmodel and Service

--- app/src/main/java/com/tugraz/studybuddy/presentation/activity/OverviewActivity.java
+++ app/src/main/java/com/tugraz/studybuddy/presentation/activity/OverviewActivity.java
@@ -1,5 +1,6 @@
 package com.tugraz.studybuddy.presentation.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -29,6 +30,8 @@ public class OverviewActivity extends AppCompatActivity {
         findViewById(R.id.button_add_course).setOnClickListener(v -> {
             Toast toast = Toast.makeText(getApplicationContext(), "Clicked add course", Toast.LENGTH_SHORT);
             toast.show();
+
+            startActivity(new Intent(this, AddCourseActivity.class));
         });
     }
 }
