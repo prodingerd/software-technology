1ec06a7ba9e48de45ae097a00aa48a85b2c57af2
Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>
2023-05-03 15:12:47 +0200

    Move classes to destined package
    Signed-off-by: Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>

---
+++ app/src/main/java/com/tugraz/studybuddy/presentation/activity/OverviewActivity.java
@@ -0,0 +1,34 @@
+package com.tugraz.studybuddy.presentation.activity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tugraz.studybuddy.R;
+import com.tugraz.studybuddy.data.model.CourseModel;
+import com.tugraz.studybuddy.data.repository.CourseRepository;
+import com.tugraz.studybuddy.data.repository.ICourseRepository;
+import com.tugraz.studybuddy.presentation.adapter.CourseAdapter;
+
+public class OverviewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+
+        ICourseRepository<CourseModel> repository = new CourseRepository();
+        RecyclerView courseRecycler = findViewById(R.id.recyclerViewMain);
+        CourseAdapter adapter = new CourseAdapter(repository.getAll());
+
+        courseRecycler.setAdapter(adapter);
+        courseRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        findViewById(R.id.button_add_course).setOnClickListener(v -> {
+            Toast toast = Toast.makeText(getApplicationContext(), "Clicked add course", Toast.LENGTH_SHORT);
+            toast.show();
+        });
+    }
+}
