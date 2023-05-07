67681921b6d8a2fcee33430eca9fa9967ba24745
Stefan Kanduth <st.kanduth@gmail.com>
2023-05-07 15:00:19 +0200

    Add CardService and CardViewModel

---
+++ app/src/main/java/com/tugraz/studybuddy/domain/service/CardService.java
@@ -0,0 +1,25 @@
+package com.tugraz.studybuddy.domain.service;
+
+
+import com.tugraz.studybuddy.data.model.CardModel;
+import com.tugraz.studybuddy.data.repository.CardRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class CardService {
+    private static final String TAG = "CardService";
+
+    private final CardRepository cardRepository;
+
+    @Inject
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public List<CardModel> getAllCards() {
+        return cardRepository.getAll();
+    }
+
+}
