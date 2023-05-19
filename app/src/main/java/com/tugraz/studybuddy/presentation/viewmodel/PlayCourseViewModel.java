561c880803d771e27d7c76c1801c08e205eaabff
Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>
2023-05-19 14:55:17 +0200

    Fix layout errors
    Signed-off-by: Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>

--- app/src/main/java/com/tugraz/studybuddy/presentation/viewmodel/PlayCourseViewModel.java
+++ app/src/main/java/com/tugraz/studybuddy/presentation/viewmodel/PlayCourseViewModel.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel;
 
 import com.tugraz.studybuddy.data.model.CardModel;
 import com.tugraz.studybuddy.domain.service.CardService;
+import com.tugraz.studybuddy.domain.service.CourseService;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
@@ -15,16 +16,19 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 @HiltViewModel
 public class PlayCourseViewModel extends ViewModel {
 
+    private final CourseService courseService;
     private final CardService cardService;
     private Queue<CardModel> cards;
 
     @Inject
-    public PlayCourseViewModel(CardService cardService) {
+    public PlayCourseViewModel(CourseService courseService, CardService cardService) {
+        this.courseService = courseService;
         this.cardService = cardService;
     }
 
     public void initialize(String courseId) {
-        this.cards = new ArrayDeque<>(cardService.getAllCards());
+        courseService.getCourseById(courseId);
+        cards = new ArrayDeque<>(cardService.getAllCards());
     }
 
     public CardModel nextCard() {
