eeeef57952e010c228ba1d9680049188e9d774ee
Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>
2023-06-13 16:15:45 +0200

    Move CourseRepository to Firestore
    Signed-off-by: Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>

--- app/src/main/java/com/tugraz/studybuddy/presentation/activity/OverviewActivity.java
+++ app/src/main/java/com/tugraz/studybuddy/presentation/activity/OverviewActivity.java
@@ -1,7 +1,6 @@
 package com.tugraz.studybuddy.presentation.activity;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -27,12 +26,12 @@ public class OverviewActivity extends AppCompatActivity implements IClickListene
         setContentView(R.layout.activity_overview);
 
         CourseViewModel courseViewModel = new ViewModelProvider(this).get(CourseViewModel.class);
-
         RecyclerView courseRecycler = findViewById(R.id.recyclerViewMain);
-        CourseAdapter adapter = new CourseAdapter(courseViewModel.getAllCourses(), this);
 
-        courseRecycler.setAdapter(adapter);
-        courseRecycler.setLayoutManager(new LinearLayoutManager(this));
+        courseViewModel.getAllCourses().observe(this, courses -> {
+            courseRecycler.setAdapter(new CourseAdapter(courses, this));
+            courseRecycler.setLayoutManager(new LinearLayoutManager(this));
+        });
 
         findViewById(R.id.button_add_course).setOnClickListener(v -> startActivity(new Intent(this, AddCourseActivity.class)));
     }
@@ -47,12 +46,12 @@ public class OverviewActivity extends AppCompatActivity implements IClickListene
     @Override
     public boolean longOnItemClick(CourseModel course) {
         CourseViewModel courseViewModel = new ViewModelProvider(this).get(CourseViewModel.class);
-        Context context = this;
-        new AlertDialog.Builder(this).setTitle(R.string.delete_title).setMessage(R.string.delete_message_course)
-                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
-                    courseViewModel.deleteCourse(course);
-                    startActivity(new Intent(context, OverviewActivity.class));
-                }).setNegativeButton(android.R.string.cancel, null).show();
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.delete_title)
+                .setMessage(R.string.delete_message_course)
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> courseViewModel.deleteCourse(course))
+                .setNegativeButton(android.R.string.cancel, null)
+                .show();
         return true;
     }
 }
