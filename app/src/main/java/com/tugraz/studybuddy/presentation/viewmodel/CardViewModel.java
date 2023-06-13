3320d6a638ae995da3fcb384f7e45c67f3c25c2f
kanduts <stefan.kanduth@student.tugraz.at>
2023-06-13 19:28:32 +0200

    Implement edit functionality

--- app/src/main/java/com/tugraz/studybuddy/presentation/viewmodel/CardViewModel.java
+++ app/src/main/java/com/tugraz/studybuddy/presentation/viewmodel/CardViewModel.java
@@ -42,4 +42,8 @@ public class CardViewModel extends ViewModel {
     public void deleteCard(CardModel card) {
         cardService.deleteCard(courseId, card);
     }
+
+    public boolean updateCard(String id, String frontText, String backText) {
+        return cardService.updateCard(id, frontText, backText);
+    }
 }
