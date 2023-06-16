abb163b1279fe2fe5e2c7a484535dab420cbf4da
Stefan Kanduth <st.kanduth@gmail.com>
2023-06-16 19:48:12 +0200

    Adjust test to changes

--- app/src/test/java/com/tugraz/studybuddy/presentation/viewmodel/PlayCourseViewModelTest.java
+++ app/src/test/java/com/tugraz/studybuddy/presentation/viewmodel/PlayCourseViewModelTest.java
@@ -1,10 +1,6 @@
 package com.tugraz.studybuddy.presentation.viewmodel;
 
-import androidx.lifecycle.MutableLiveData;
-
-import com.tugraz.studybuddy.data.model.CourseModel;
 import com.tugraz.studybuddy.domain.service.CardService;
-import com.tugraz.studybuddy.domain.service.CourseService;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,8 +10,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.List;
-
 @RunWith(MockitoJUnitRunner.class)
 public class PlayCourseViewModelTest {
 
@@ -23,13 +17,11 @@ public class PlayCourseViewModelTest {
 
     @Mock
     private CardService cardServiceMock;
-    private CourseService courseServiceMock;
 
     @Before
     public void setUp() {
         cardServiceMock = Mockito.mock(CardService.class);
-        courseServiceMock = Mockito.mock(CourseService.class);
-        playCourseViewModel = new PlayCourseViewModel(courseServiceMock, cardServiceMock);
+        playCourseViewModel = new PlayCourseViewModel(cardServiceMock);
     }
 
     @Test
