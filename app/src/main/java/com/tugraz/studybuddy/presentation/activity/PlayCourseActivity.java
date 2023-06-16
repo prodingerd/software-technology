f283cc415ea4b6ea85bae7faea31c7041cc11dba
Stefan Kanduth <st.kanduth@gmail.com>
2023-06-16 18:58:34 +0200

    Fix PlayCourse also change Button style

--- app/src/main/java/com/tugraz/studybuddy/presentation/activity/PlayCourseActivity.java
+++ app/src/main/java/com/tugraz/studybuddy/presentation/activity/PlayCourseActivity.java
@@ -12,11 +12,13 @@ import com.tugraz.studybuddy.data.model.CardModel;
 import com.tugraz.studybuddy.data.model.CourseModel;
 import com.tugraz.studybuddy.presentation.viewmodel.PlayCourseViewModel;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class PlayCourseActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +29,15 @@ public class PlayCourseActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         CourseModel course = (CourseModel) extras.get("course");
 
-        playCourseViewModel.initialize(course.getId());
-
         TextView frontCardText = findViewById(R.id.textViewFront);
         TextView backCardText = findViewById(R.id.textViewBack);
-        CardModel currentCard = playCourseViewModel.nextCard();
-        frontCardText.setText(currentCard.getFrontText());
-        backCardText.setText(currentCard.getBackText());
-        backCardText.setVisibility(View.INVISIBLE);
+
+        Queue<CardModel> cardQueue = new ArrayDeque<>();
+
+        playCourseViewModel.initialize(course.getId()).observe(this, collection -> {
+            cardQueue.addAll(collection);
+            displayNextCard(cardQueue);
+        });
 
         frontCardText.setOnClickListener(v -> {
             if (backCardText.getVisibility() == View.INVISIBLE) {
@@ -44,11 +47,18 @@ public class PlayCourseActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.buttonNextCard).setOnClickListener(v -> {
-            CardModel nextCard = playCourseViewModel.nextCard();
+        findViewById(R.id.buttonNextCard).setOnClickListener(v -> displayNextCard(cardQueue));
+    }
+
+    private void displayNextCard(Queue<CardModel> cards){
+        TextView frontCardText = findViewById(R.id.textViewFront);
+        TextView backCardText = findViewById(R.id.textViewBack);
+
+        CardModel nextCard = cards.poll();
+        if(nextCard != null) {
             frontCardText.setText(nextCard.getFrontText());
             backCardText.setText(nextCard.getBackText());
             backCardText.setVisibility(View.INVISIBLE);
-        });
+        }
     }
 }
