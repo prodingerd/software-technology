85c1b2869b763056ef0be3bc54b1e0449c6e7236
maierbrf <fabio.maierbrugger@student.tugraz.at>
2023-06-18 17:37:22 +0200

    first tries share, Failure getting documents                                                                                                     com.google.firebase.firestore.FirebaseFirestoreException: PERMISSION_DENIED: Missing or insufficient permissions.

--- app/src/main/java/com/tugraz/studybuddy/domain/service/CourseService.java
+++ app/src/main/java/com/tugraz/studybuddy/domain/service/CourseService.java
@@ -3,10 +3,12 @@ package com.tugraz.studybuddy.domain.service;
 import androidx.lifecycle.MutableLiveData;
 
 import com.tugraz.studybuddy.data.model.CourseModel;
+import com.tugraz.studybuddy.data.model.SharedCourseModel;
 import com.tugraz.studybuddy.data.repository.CourseRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
@@ -44,7 +46,27 @@ public class CourseService {
         return true;
     }
 
+    public String shareCourse(String courseId){
+        List<SharedCourseModel> sharedCourses =  repository.getAllSharedCourses();
+        for (SharedCourseModel sharedCourse : sharedCourses) {
+            if(sharedCourse.getCourseId().equals(courseId)){
+                return sharedCourse.getCode();
+            }
+        }
+        if(!repository.checkIfCourseId(courseId)){
+
+            return null;
+        }
+        String code = random();
+        repository.addSharedCourse(new SharedCourseModel(courseId, code));
+        return code;
+    }
+
     public void deleteCourse(CourseModel course) {
         repository.delete(course);
+    }
+
+    public static String random() {
+        return UUID.randomUUID().toString().substring(0,5);
     }
 }
