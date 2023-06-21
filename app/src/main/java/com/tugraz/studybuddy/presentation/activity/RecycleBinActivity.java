f7b3f72bbbebb8b38b85eba15af36a232b69be8c
Benjamin Jost <bnj.jost@gmail.com>
2023-06-21 09:36:33 +0200

    Add recyle bin fab

---
+++ app/src/main/java/com/tugraz/studybuddy/presentation/activity/RecycleBinActivity.java
@@ -0,0 +1,16 @@
+package com.tugraz.studybuddy.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tugraz.studybuddy.R;
+
+public class RecycleBinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_bin);
+    }
+}
